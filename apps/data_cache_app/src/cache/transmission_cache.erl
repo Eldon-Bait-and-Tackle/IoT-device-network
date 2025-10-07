9a7da669aa3e0b21f6aba9a35e07bddbe0b43e81
Eldon <Birdheldon@gmail.com>
2025-10-07 14:11:28 -0600

    data and cache modules
    simply adding the modules into the repo for the data and cache, I need to get ETS working and then the database handler.

---
+++ apps/data_cache_app/src/cache/transmission_cache.erl
@@ -0,0 +1,46 @@
+﻿%%%-------------------------------------------------------------------
+%%% @author Eldon
+%%% @copyright (C) 2025, <COMPANY>
+%%% @doc
+%%% @end
+%%%-------------------------------------------------------------------
+-module(transmission_cache).
+
+-behaviour(gen_server).
+
+-export([start_link/0]).
+-export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2,
+    code_change/3]).
+
+-define(SERVER, ?MODULE).
+
+-record(transmission_cache_state, {}).
+
+%%%===================================================================
+%%% Spawning and gen_server implementation
+%%%===================================================================
+
+start_link() ->
+    gen_server:start_link({local, ?SERVER}, ?MODULE, [], []).
+
+init([]) ->
+    {ok, #transmission_cache_state{}}.
+
+handle_call(_Request, _From, State = #transmission_cache_state{}) ->
+    {reply, ok, State}.
+
+handle_cast(_Request, State = #transmission_cache_state{}) ->
+    {noreply, State}.
+
+handle_info(_Info, State = #transmission_cache_state{}) ->
+    {noreply, State}.
+
+terminate(_Reason, _State = #transmission_cache_state{}) ->
+    ok.
+
+code_change(_OldVsn, State = #transmission_cache_state{}, _Extra) ->
+    {ok, State}.
+
+%%%===================================================================
+%%% Internal functions
+%%%===================================================================
