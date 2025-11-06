5369e6d9e1fbf117f30f9c86de9f64841b438c5a
Eldon <Birdheldon@gmail.com>
2025-11-05 23:30:07 -0700

    added logging service
    added the logging module, this should be good for replacing all err messages into this log file in the future. Consider replacing all other err messages with a log message as a good steward ship practice, thanks!

---
+++ test/hsn_api_SUITE.erl
@@ -0,0 +1,15 @@
+%%%-------------------------------------------------------------------
+%%% @author Eldon
+%%% @copyright (C) 2025, <COMPANY>
+%%% @doc
+%%%
+%%% @end
+%%% Created : 05. Nov 2025 23:15
+%%%-------------------------------------------------------------------
+-module(hsn_api_SUITE).
+-author("Eldon").
+
+-include_lib("eunit/include/eunit.hrl").
+
+simple_test() ->
+    ?assert(true).
